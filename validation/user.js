937294ea50835e5fb932d88a34b4ce7cb0562196
patelkarishma10 <patelkarishma10@gmail.com>
2019-07-26 12:04:52 +0100

    made it so that password is validated when creating account (alos got part 2 and 3 mixed up)

--- validation/user.js
+++ validation/user.js
@@ -5,6 +5,7 @@ let errors = {};
  // if these values are not present in the data object we are validating then they will be set to empty strings for the Validator.isEmpty
 user.username = !isEmpty(user.username) ? user.username : "";
 user.password = !isEmpty(user.password) ? user.password : "";
+user.password2 = !isEmpty(user.password2) ? user.password2 : "";
 user.email = !isEmpty(user.email) ? user.email : "";
 
  //Username validation rules
@@ -19,6 +20,14 @@ errors.username = "Username field is required";
 //Password validation rules
 if (Validator.isEmpty(user.password)) {
 errors.password = "password field is required";
+}
+
+if (Validator.isEmpty(user.password2)) {
+errors.password = "password field is required";
+}
+
+if (!Validator.equals(user.password, user.password2)) {
+errors.password = "password fields do not match";
 }
 
 //Email validation rules
